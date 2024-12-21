ad518613c560e91c465ae7e1275f00eca97b7275
andrawicaksono <andrawicaksono14@gmail.com>
2024-12-22 01:38:59 +0700

    fix: change env

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,23 +2,15 @@ const firebase = require("firebase/app");
 const firestore = require("firebase/firestore");
 const fireauth = require("firebase/auth");
 const admin = require("firebase-admin");
+require("dotenv").config();
 
 admin.initializeApp({
   credential: admin.credential.cert(
-    require(process.env.FIREBASE_SERVICE_ACCOUNT_PATH)
+    JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
   ),
 });
 
-require("dotenv").config();
-
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-};
+const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG);
 
 const app = firebase.initializeApp(firebaseConfig);
 const db = firestore.getFirestore(app);
