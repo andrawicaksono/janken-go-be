0a37457963fa30d49e9b727d6e2f2bcceeb93a57
andrawicaksono <andrawicaksono14@gmail.com>
2024-12-23 17:20:22 +0700

    fix: online multiplayer

--- src/socket.js
+++ src/socket.js
@@ -1,160 +1,75 @@
 const Socket = (io) => {
-  let rooms = {};
+  const rooms = {};
 
   io.on("connection", (socket) => {
     console.log("A user connected:", socket.id);
 
-    socket.on("disconnect", () => {
-      console.log("User disconnected:", socket.id);
-      for (const roomCode in rooms) {
-        if (
-          rooms[roomCode].host === socket.id ||
-          rooms[roomCode].guest === socket.id
-        ) {
-          delete rooms[roomCode];
-          io.to(roomCode).emit("roomClosed");
-          break;
+    socket.on("createRoom", (roomId) => {
+      if (!rooms[roomId]) {
+        rooms[roomId] = {
+          players: [],
+          moves: {},
+          scores: { player1: 0, player2: 0 },
+        };
+        socket.join(roomId);
+        rooms[roomId].players.push(socket.id);
+        socket.emit("roomCreated", roomId);
+        console.log("Room created:", roomId);
+      }
+    });
+
+    // Handle joining a room
+    socket.on("joinRoom", (roomId) => {
+      if (rooms[roomId] && rooms[roomId].players.length < 2) {
+        socket.join(roomId);
+        rooms[roomId].players.push(socket.id);
+        io.to(roomId).emit("playerJoined", rooms[roomId].players);
+      } else {
+        socket.emit("error", "Room is full or does not exist.");
+      }
+    });
+
+    // Handle player move
+    socket.on("playerMove", ({ roomId, move }) => {
+      const room = rooms[roomId];
+      if (room) {
+        room.moves[socket.id] = move;
+
+        if (Object.keys(room.moves).length === 2) {
+          const [player1, player2] = room.players;
+          const result = determineWinner(
+            room.moves[player1],
+            room.moves[player2]
+          );
+
+          if (result === "player1") room.scores.player1++;
+          else if (result === "player2") room.scores.player2++;
+
+          io.to(roomId).emit("roundResult", {
+            moves: room.moves,
+            result,
+            scores: room.scores,
+          });
+
+          room.moves = {};
         }
       }
     });
 
-    socket.on("createGame", () => {
-      const roomCode = generateRoomCode(6);
-      rooms[roomCode] = {
-        host: socket.id,
-        guest: null,
-        p1moves: [],
-        p2moves: [],
-        round: 1,
-        scores: { p1: 0, p2: 0 },
-        p1Wins: 0,
-        p2Wins: 0,
-        gameOver: false,
-      };
-      socket.join(roomCode);
-      socket.emit("newGame", { roomCode });
-    });
-
-    socket.on("joinGame", (data) => {
-      const room = rooms[data.roomCode];
-      if (!room) {
-        socket.emit("roomNotFound");
-        return;
+    socket.on("disconnect", () => {
+      console.log("User disconnected:", socket.id);
+      for (const roomId in rooms) {
+        const room = rooms[roomId];
+        room.players = room.players.filter((player) => player !== socket.id);
+        if (room.players.length === 0) delete rooms[roomId];
       }
-      if (room.guest) {
-        socket.emit("roomFull");
-        return;
-      }
-      if (room.host === socket.id) {
-        socket.emit("alreadyInRoom");
-        return;
-      }
-
-      room.guest = socket.id;
-      socket.join(data.roomCode);
-      io.to(data.roomCode).emit("startGame", room);
-    });
-
-    socket.on("p1Choice", (data) => {
-      const room = rooms[data.roomCode];
-      if (!room || room.gameOver) {
-        socket.emit("invalidRoom");
-        return;
-      }
-      if (!isValidChoice(data.rpsValue)) {
-        socket.emit("invalidChoice");
-        return;
-      }
-
-      room.p1moves.push(data.rpsValue);
-      checkAndDeclareWinner(data.roomCode);
-    });
-
-    socket.on("p2Choice", (data) => {
-      const room = rooms[data.roomCode];
-      if (!room || room.gameOver) {
-        socket.emit("invalidRoom");
-        return;
-      }
-      if (!isValidChoice(data.rpsValue)) {
-        socket.emit("invalidChoice");
-        return;
-      }
-
-      room.p2moves.push(data.rpsValue);
-      checkAndDeclareWinner(data.roomCode);
-    });
-
-    socket.on("nextRound", (data) => {
-      const room = rooms[data.roomCode];
-      if (!room || room.gameOver) {
-        socket.emit("invalidRoom");
-        return;
-      }
-
-      room.round += 1;
-      io.to(data.roomCode).emit("nextRound", room);
     });
   });
 
-  function checkAndDeclareWinner(roomCode) {
-    const room = rooms[roomCode];
-    if (
-      room.p1moves.length === room.round &&
-      room.p2moves.length === room.round
-    ) {
-      const p1Choice = room.p1moves[room.round - 1];
-      const p2Choice = room.p2moves[room.round - 1];
-      const winner = getWinner(p1Choice, p2Choice);
-
-      if (winner === "p1") room.p1Wins += 1;
-      if (winner === "p2") room.p2Wins += 1;
-
-      io.sockets.to(roomCode).emit("result", {
-        winner,
-        p1Choice,
-        p2Choice,
-        scores: room.scores,
-        round: room.round,
-        p1moves: room.p1moves,
-        p2moves: room.p2moves,
-        p1Wins: room.p1Wins,
-        p2Wins: room.p2Wins,
-      });
-
-      if (room.p1Wins === 3) {
-        room.gameOver = true;
-        io.sockets.to(roomCode).emit("gameOver", { winner: "p1" });
-      } else if (room.p2Wins === 3) {
-        room.gameOver = true;
-        io.sockets.to(roomCode).emit("gameOver", { winner: "p2" });
-      }
-    }
-  }
-
-  function generateRoomCode(length) {
-    let result = "";
-    const characters = "0123456789";
-    for (let i = 0; i < length; i++) {
-      result += characters.charAt(
-        Math.floor(Math.random() * characters.length)
-      );
-    }
-    return result;
-  }
-
-  function isValidChoice(choice) {
-    return ["Rock", "Paper", "Scissor", ""].includes(choice);
-  }
-
-  function getWinner(p1Choice, p2Choice) {
-    if (p1Choice === p2Choice) return "d";
-    const rules = {
-      Rock: "Scissor",
-      Scissor: "Paper",
-      Paper: "Rock",
-    };
-    return rules[p1Choice] === p2Choice ? "p1" : "p2";
+  function determineWinner(move1, move2) {
+    const rules = { Rock: "Scissor", Scissor: "Paper", Paper: "Rock" };
+    if (move1 === move2) return "draw";
+    return rules[move1] === move2 ? "player1" : "player2";
   }
 };
 
